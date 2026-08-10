086dd2bbd9f0ff0aa1a959dee8c917c825597683
MarcoHantel <hantelmarco@gmailcom>
2026-08-10 17:05:47 +0200

    create new component for create a new survey, update app.routes.ts and add routerOutlet & routerLink at the ts and html new-survey.component.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { LandingpageComponent } from './pages/landingpage/landingpage.component';
+import { CreateSurveyComponent } from './pages/create-survey/create-survey.component';
 
 export const routes: Routes = [
 
-    { path:'', component: LandingpageComponent }
+    { path:'', component: LandingpageComponent },
+
+    { path:'create-survey', component: CreateSurveyComponent}
 
 ];
--- src/app/pages/landingpage/new-survey/new-survey.component.ts
+++ src/app/pages/landingpage/new-survey/new-survey.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-new-survey',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterOutlet
+  ],
   templateUrl: './new-survey.component.html',
   styleUrl: './new-survey.component.scss'
 })
