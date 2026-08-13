c1d37eadcaaf4abac0969d7b4511e770cdf05000
MarcoHantel <hantelmarco@gmailcom>
2026-08-13 14:34:33 +0200

    start setting up the from survye

--- src/app/pages/create-survey/create-survey.component.ts
+++ src/app/pages/create-survey/create-survey.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-survey',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create-survey.component.html',
   styleUrl: './create-survey.component.scss'
 })
 export class CreateSurveyComponent {
+
+  surveyName = new FormControl('',{
+    validators:[Validators.required, Validators.minLength(4)]
+  });
+
+  // setzt meinen Wert wieder aud 'leer'
+  updateInput(){
+    this.surveyName.setValue('');
+  }
+
+    // setzt meinen Wert wieder aud 'leer'
+  deleteInput(){
+    this.surveyName.setValue('');
+  }
 
 }
