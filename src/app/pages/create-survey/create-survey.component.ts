c305270ba080e2f23f0c8152aaa1fef5a98e7f31
MarcoHantel <hantelmarco@gmailcom>
2026-08-18 11:36:00 +0200

    create function to set default value for creat new survey date and start designing the input feelds fromm the survey

--- src/app/pages/create-survey/create-survey.component.ts
+++ src/app/pages/create-survey/create-survey.component.ts
@@ -8,18 +8,53 @@ import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
   styleUrl: './create-survey.component.scss'
 })
 export class CreateSurveyComponent {
+  questions = 1;
 
-  surveyName = new FormControl('',{
-    validators:[Validators.required, Validators.minLength(4)]
+  // FormControl 
+  surveyName = new FormControl('', {
+    validators: [Validators.required, Validators.minLength(4)]
   });
 
-  // setzt meinen Wert wieder aud 'leer'
-  updateInput(){
-    this.surveyName.setValue('');
+  surveyEndDate = new FormControl('');
+
+  surveyQuestions = new FormControl('', {
+    validators: [Validators.required, Validators.minLength(6)]
+  });
+
+  surveyAnswer = new FormControl('', {
+    validators: [Validators.required]
+  });
+
+  surveyDescription = new FormControl('', {
+    validators: [Validators.required, Validators.maxLength(500)]
+  });
+
+  // check ob Enddatum da, wenn nicht nehme das heutige + 24 Stunden
+  checkEndDate() {
+    if (!this.surveyEndDate.value) {
+
+      const defaultDate = new Date(Date.now() + 24 * 60 * 60 * 1000);
+      const formatted = defaultDate.toISOString().split('T')[0]; // Datum in das Format YYYY-MM-DD umwandeln (was input type="date" braucht)
+      this.surveyEndDate.setValue(formatted);       // ← ins FormControl setzen!
+
+      console.log(formatted);
+      // Keine else notwendig wenn Datum vom User eigeben wurde.
+    }
   }
 
-    // setzt meinen Wert wieder aud 'leer'
-  deleteInput(){
+  // setzt meinen Wert wieder aud 'leer' wenn click auf submit 
+  updateInput() {
+    this.checkEndDate();
+
+    this.surveyName.setValue('');
+    this.surveyEndDate.setValue('');
+    this.surveyQuestions.setValue('');
+    this.surveyAnswer.setValue('');
+    this.surveyDescription.setValue('');
+  }
+
+  // setzt meinen Wert wieder aud 'leer' click auf löschen
+  deleteInput() {
     this.surveyName.setValue('');
   }
 
